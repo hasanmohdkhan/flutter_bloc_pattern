e10a78d9edc8c3ba3d87094b1ecf84b14e16dbc1
HasanZian <hasanzian@gmail.com>
2020-11-03 19:47:42 +0530

    added providers and bloc for screen

--- lib/UI/main_screen.dart
+++ lib/UI/main_screen.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:restaurant_finder/UI/location_screen.dart';
+import 'package:restaurant_finder/BLoC/bloc_provider.dart';
+import 'package:restaurant_finder/BLoC/location_bloc.dart';
+import 'package:restaurant_finder/DataLayer/location.dart';
+import 'package:restaurant_finder/UI/restaurant_screen.dart';
+
+import 'location_screen.dart';
 
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return LocationScreen();
+    return StreamBuilder<Location>(
+        stream: BlocProvider.of<LocationBloc>(context).locationStream,
+        builder: (context, snapshot) {
+          final location = snapshot.data;
+
+          if (location == null) {
+            return LocationScreen();
+          }
+
+          return RestaurantScreen(location: location);
+        });
   }
 }
